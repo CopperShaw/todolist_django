7926a46fb1f60ea47f5581e42b5982eb19a145e6
Andrei Constantin <constantinandreivalentin27@gmail.com>
2023-01-06 15:36:22 +0200

    initial commit

---
+++ todo_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from todo_app.models import ToDoItem, ToDoList
+
+# Register your models here.
+
+admin.site.register(ToDoList)
+admin.site.register(ToDoItem)
---
+++ todo_app/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.ToDoListView.as_view(), name="index"),
+    path("list/<int:list_id>/", views.ItemListView.as_view(), name="list"),
+    path("list/add/", views.ListCreateView.as_view(), name="list-add"),
+    path("list/<int:pk>/delete/", views.ListDeleteView.as_view(), name="list-delete"),
+    path(
+        "list/<int:list_id>/item/add/",
+        views.ItemCreateView.as_view(),
+        name="item-add",
+    ),
+    path(
+        "list/<int:list_id>/item/<int:pk>/",
+        views.ItemUpdateView.as_view(),
+        name="item-update",
+    ),
+    path(
+        "list/<int:list_id>/item/<int:pk>/delete/",
+        views.ItemDeleteView.as_view(),
+        name="item-delete",
+    ),
+]
